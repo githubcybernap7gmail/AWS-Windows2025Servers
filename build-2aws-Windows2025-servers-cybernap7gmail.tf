b7d1df6a321042439b02af408d7176a661480a9a
githubcybernap7gmail <88942076+githubcybernap7gmail@users.noreply.github.com>
2025-10-04 14:50:24 -0400

    Update build-2aws-Windows2025-servers-cybernap7gmail.tf

--- build-2aws-Windows2025-servers-cybernap7gmail.tf
+++ build-2aws-Windows2025-servers-cybernap7gmail.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "NapUbuntuServer1" {
+resource "aws_instance" "NapWindows2025Server1" {
   ami = "ami-0e3c2921641a4a215"
   instance_type = "t2.micro"
   availability_zone = "us-east-1a"
